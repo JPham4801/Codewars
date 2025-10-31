f18419fe5194b53864a59bff5e5c198fadcec934
John Pham <JPham4801@gmail.com>
2025-10-31 13:10:42 -0500

    Update solution - CodeHub

--- 8-kyu/find-the-first-non-consecutive-number/find-the-first-non-consecutive-number.js
+++ 8-kyu/find-the-first-non-consecutive-number/find-the-first-non-consecutive-number.js
@@ -1,6 +1,6 @@
  
 function firstNonConsecutive(arr){
-  for(let i = 0; i < arr.length-1; i++){
+  for(let i = 0; i < arr.length - 1; i++){
     if(arr[i] + 1 !== arr[i + 1]){
       console.log(arr[i], arr[i + 1]);
       return arr[i + 1];
