cfd866b5556d5e124a2ec1df0890395fdc061edb
John Pham <JPham4801@gmail.com>
2025-11-02 16:29:35 -0600

    Update solution - CodeHub

--- 7-kyu/remove-the-minimum/remove-the-minimum.js
+++ 7-kyu/remove-the-minimum/remove-the-minimum.js
@@ -1,8 +1,4 @@
- 
 function removeSmallest(numbers) {
-//   if(numbers.length > 0){
-//     return [];
-//   }
   let num = numbers.slice()
   let pos = num.indexOf(Math.min(...numbers))
   num.splice(pos, 1)  
